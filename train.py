4d065bc482f8b9803a964afa66d4aa747accf9db
Robert Clark <87581984+RobertWClark123@users.noreply.github.com>
2021-07-17 16:16:40 -0400

    Update train.py

--- train.py
+++ train.py
@@ -32,13 +32,51 @@ def make_model():
   
 def make_datagens():
   
-  train_datagen = ImageDataGenerator(
-    rescale=1./255,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=False)
+    train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=False)
   
-  test_datagen = ImageDataGenerator(rescale=1.255)
+    test_datagen = ImageDataGenerator(rescale=1.255)
   
-  train_generator = train_datagen.flow_from_directory(
+    train_generator = train_datagen.flow_from_directory(
+        'GTSRB/Final_training/Images',
+        target_size=(32, 32),
+        batch_size=128, #originally 16, then 64
+        class_mode='categorical')
     
+    validation_generator = test_datagen.flow_from_directory(
+        'GTSRB/Test_Arranged',
+        target_size=(32,32),
+        batch_size=128, #originally 16, then 64
+        class_mode='categorical')
+    return train_generator, validation_generator
+
+def train_model():
+    
+    train_generator, validation_generator = make_datagens()
+    model = make_model()
+    
+    # checkpoint
+    filepath="weights-improvement-{epocj:02d}-{val_accuracy:.4f}.hdf5"
+    checkpoint = ModelCheckpoint(filepath, monitor='val_accuracy', verbose=1, save_best_only=False, mode='max')
+    callbacks_list = [checkpoint]
+    
+    model.fit_generator(
+        train_generator,
+        steps_per_epoch=256,
+        epochs=256,
+        callbacks=callbacks_list,
+        validation_data=validation_generator,
+        validation_steps=80)
+    
+    Y_pred = model.predict_generator(validation_generator, steps_per_epoch)
+    y_pred = np.argmax(Y_pred, axis=1)
+    print('Confustion Matrix')
+    print(confusion_matrix(validation_generator.class, y_pred))
+    print('Classification Report')
+    target_names=['1', '2', '3']
+    print(classification_report(validation_generator.classes, y_pred, target_names=target_names))
+    
+train_model()
